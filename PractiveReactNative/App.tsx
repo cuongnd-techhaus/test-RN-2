a79b55d46034eed55d936a140797f399937ec955
duycuongit <118497439+duycuongit@users.noreply.github.com>
2023-04-02 16:31:42 +0700

    modify state

--- PractiveReactNative/App.tsx
+++ PractiveReactNative/App.tsx
@@ -38,13 +38,7 @@ import PresentationalComponent from './PresentationalComponent';
 export default class App extends React.Component {
   state = {
     myState:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed ' +
-      'do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
-      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi' +
-      'ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit ' +
-      'in voluptate velit esse cillum dolore eu fugiat nulla pariatur.' +
-      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia' +
-      'deserunt mollit anim id est laborum.'
+      'This is my state'
   };
   updateState = () => {
     this.setState({myState: 'The state is updated'});
@@ -53,10 +47,7 @@ export default class App extends React.Component {
     return (
       <ScrollView>
         <View>
-          <PresentationalComponent
-            myState={this.state.myState}
-            updateState={this.updateState}
-          />
+          <PresentationalComponent myState={this.state.myState}></PresentationalComponent>
         </View>
       </ScrollView>
     );
